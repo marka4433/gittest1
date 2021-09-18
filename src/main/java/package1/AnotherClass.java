ed96934df3e39d297a771d644fda1cc3e588b2b6
VanVan <marka4433@gmail.com>
2021-09-18 20:39:57 +0300

    some new logic is created

--- src/main/java/package1/AnotherClass.java
+++ src/main/java/package1/AnotherClass.java
@@ -5,6 +5,5 @@ public class AnotherClass {
     public void doSmth(){
         System.out.println("Gagaga");
         System.out.println("Gagaga2");
-        System.out.println("Gagaga2");
     }
 }
